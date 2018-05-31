d87a85b2c942ed5b43be55789e54d5db27254cd0
ZG038525H\edson.machado <edson.machado@grupocmc.es>
2018-05-31 12:57:39 +0200

    Navigating programmatically

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
-import { Link } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
 import './Posts.css';
@@ -32,7 +31,7 @@ class Posts extends Component {
   }
 
   postSelectedHandler = (id) => {
-    this.setState({selectedPostId: id});
+    this.props.history.push({pathname: '/' + id});
   }
     
   render () {
@@ -40,12 +39,13 @@ class Posts extends Component {
     if (!this.state.error) {
       posts = this.state.posts.map(post => {
         return (
-          <Link key={post.id} to={'/' + post.id}>
+          // <Link key={post.id} to={'/' + post.id}>
             <Post 
+              key={post.id}
               title={post.title} 
               author={post.author}
               clicked={() => this.postSelectedHandler(post.id)} />
-          </Link>
+          // </Link>
         ); 
       });
     }
